33c6223213a14dd42eb74cfc38614eeccbec2ebd
iagoesp <iago.esp@gmail.com>
2019-10-01 19:11:34 -0300

    doesn't work, dam

--- is.py
+++ is.py
@@ -6,9 +6,11 @@ import matplotlib
 import nibabel as nib
 import numpy as np
 import glob as glob
+import pydicom
 from os.path import basename
 from scipy import interpolate
 from glob import glob
+
 
 matplotlib.interactive(True)
 
@@ -20,7 +22,7 @@ def encontrar_arq(caminho):
         print("end da pasta atual: {}".format(pastaAtual))
         print("end da subPasta: {}".format(subPasta))
         for file in arq:
-            if '.nii' in file:
+            if '.dcm' in file:
                 files.append(os.path.join(pastaAtual, file))
 
     print("size = {}".format(len(files)))
@@ -44,12 +46,18 @@ print("resp 0: {}".format(resp[0]))
 print("resp 1: {}".format(resp[1]))
 
 #Example for two images
-img_filepath = resp[0]
-img_mask_filepath = resp[1]
+img_filepath = pydicom.dcmread(resp[0])
+img_mask_filepath = pydicom.dcmread(resp[1])
 print(basename(resp[0]))
 base = basename(resp[0]).split(".")
 output_filepath = os.path.join('',''+ base[0], 'img')
-os.makedirs(output_filepath)
+try:
+    os.makedirs(output_filepath)
+except:
+    os.rmdir(output_filepath)
+    os.makedirs(output_filepath)
+
+
 print(output_filepath)
 print("done")
 PC1 = 0.2
@@ -59,10 +67,10 @@ S1 = 1500
 S2 = 9500
 
 # Reads input image and mask
-input_img = nib.load(img_filepath)
-input_data = input_img.get_data()
+#input_img = nib.load(img_filepath)
+input_data = img_filepath.pixel_array
 
-input_mask = nib.load(img_mask_filepath).get_data()
+input_mask = img_mask_filepath.pixel_array
 
 # Separate brain
 input_brain = input_data[input_mask.astype(np.bool)]
@@ -80,9 +88,9 @@ f = interpolate.interp1d(input_pcs, std_pcs, kind="linear", bounds_error=False, 
 std_input = f(input_data) * input_mask
 
 # Saves image
-std_img = nib.Nifti1Image(std_input, input_img.affine, input_img.header)
+#std_img = nib.Nifti1Image(std_input, input_img.affine, input_img.header)
 #print(std_img)
-nib.save(std_img, output_filepath)
+#nib.save(std_img, output_filepath)
 
 input_img = nib.load(img_filepath)
 input_data = input_img.get_data()
@@ -94,11 +102,22 @@ input_data2 = input_img.get_data()
 input_data2 = np.rot90(input_data2)
 np.shape(input_data2)
 
-cv2.imshow("image", input_data)
-cv2.imshow("image2", input_data2)
+height = input_data.shape[0]
+windowScale = height/1000
+print (windowScale)
+
+newX,newY = input_data.shape[1]/windowScale, input_data.shape[0]/windowScale
+
+imgID1 = cv2.resize(input_data, (int(newX), int(newY)))
+imgID2 = cv2.resize(input_data2, (int(newX), int(newY)))
+
+
+cv2.imshow("image", imgID1)
+cv2.imshow("image2", imgID2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+#https://pydicom.github.io/pydicom/stable/auto_examples/input_output/plot_write_dicom.html
 
 #Acknowledgment
 #Eduardo Nigri
